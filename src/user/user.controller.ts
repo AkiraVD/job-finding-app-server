db83736d523c17f6cf59f6b0f323ef603fc37983
AkiraVD <ledanhphuong@gmail.com>
2023-03-21 20:28:57 +0700

    Upload

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+@Controller('user')
+export class UserController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Req() req: Request | any) {
+    console.log({
+      user: req.user,
+    });
+    return 'user information';
+  }
+}
