03cd636a874164dfb6a693a2d1121d380ba56bef
AkiraVD <ledanhphuong@gmail.com>
2023-03-26 20:18:48 +0700

    Config gigs swaggerr

--- src/gigs/dto/update-gig.dto.ts
+++ src/gigs/dto/update-gig.dto.ts
@@ -1,35 +1,76 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateGigDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'Title of the gig',
+    example: 'Professional Logo Design',
+  })
   title?: string;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: 'ID of the job associated with the gig',
+    example: 1,
+  })
   jobId?: number;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: 'Rate of the gig',
+    example: 5,
+  })
   rate?: number;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: 'Price of the gig',
+    example: 100,
+  })
   price?: number;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'URL of the picture for the gig',
+    example: 'https://example.com/logo.png',
+  })
   picture?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'Description of the gig',
+    example: 'I will design a professional logo for your business',
+  })
   description?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    type: String,
+    description: 'Short description of the gig',
+    example: 'Professional Logo Design',
+  })
   descShort?: string;
 
   @IsOptional()
   @IsNumber()
+  @ApiProperty({
+    type: Number,
+    description: 'Number of stars for the gig',
+    example: 4,
+  })
   stars?: number;
 }
--- src/gigs/gigs.controller.ts
+++ src/gigs/gigs.controller.ts
@@ -10,35 +10,36 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
-import { SearchDto } from '../utils';
+import { SearchDto, SearchDtoNoName } from '../utils';
 import { CreateGigDto, UpdateGigDto } from './dto';
 import { GigsService } from './gigs.service';
 
+@ApiTags('Gigs')
 @Controller('gigs')
 export class GigsController {
   constructor(private gigsService: GigsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all gigs (limit to 50)' })
   getGigs() {
     return this.gigsService.getGigs();
   }
 
-  @Get('pagination')
-  getGigsPagination(@Query() dto: SearchDto) {
-    let { item, page } = dto;
-    return this.gigsService.getGigsPagination(item, page);
-  }
-
   @Post()
   @UseGuards(JwtGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new gig' })
   createGig(@GetUser('id') userId: number, @Body() dto: CreateGigDto) {
     return this.gigsService.createGig(userId, dto);
   }
 
   @Patch(':id')
   @UseGuards(JwtGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update an existing gig' })
   updateGig(
     @GetUser('id', ParseIntPipe) userId: number,
     @Param('id', ParseIntPipe) gigId: number,
@@ -49,6 +50,8 @@ export class GigsController {
 
   @Delete(':id')
   @UseGuards(JwtGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete an existing gig' })
   deleteGigs(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) gigId: number,
@@ -56,44 +59,63 @@ export class GigsController {
     return this.gigsService.deleteGig(userId, gigId);
   }
 
+  @Get('pagination')
+  @ApiOperation({ summary: 'Get gigs with pagination' })
+  getGigsPagination(@Query() dto: SearchDtoNoName) {
+    let { item, page } = dto;
+    return this.gigsService.getGigsPagination(item, page);
+  }
+
+  @ApiOperation({ summary: 'Get gig by ID' })
+  @ApiBearerAuth()
   @Get('id=:id')
   getGigsById(@Param('id', ParseIntPipe) id: number) {
     return this.gigsService.getGigById(id);
   }
 
+  @ApiOperation({ summary: 'Get gig details by ID' })
+  @ApiBearerAuth()
   @Get('detail/:id')
   getGigDetails(@Param('id', ParseIntPipe) id: number) {
     return this.gigsService.getGigDetailsById(id);
   }
 
+  @ApiOperation({ summary: 'Search gigs by name' })
+  @ApiBearerAuth()
   @Get('search')
   searchGigsByName(@Query() dto: SearchDto) {
     let { item, page, name } = dto;
     return this.gigsService.searchGigsByName(item, page, name);
   }
 
+  @ApiOperation({ summary: 'Get gigs by category' })
+  @ApiBearerAuth()
   @Get('category/:categoryId')
   getGigsByCategory(
     @Param('categoryId', ParseIntPipe) categoryId: number,
-    @Query() dto: SearchDto,
+    @Query() dto: SearchDtoNoName,
   ) {
     let { item, page } = dto;
     return this.gigsService.getGigsByCategory(categoryId, item, page);
   }
 
+  @ApiOperation({ summary: 'Get gigs by job' })
+  @ApiBearerAuth()
   @Get('job/:jobId')
   getGigsByJob(
     @Param('jobId', ParseIntPipe) jobId: number,
-    @Query() dto: SearchDto,
+    @Query() dto: SearchDtoNoName,
   ) {
     let { item, page } = dto;
     return this.gigsService.getGigsByJob(jobId, item, page);
   }
 
+  @ApiOperation({ summary: 'Get gigs by user' })
+  @ApiBearerAuth()
   @Get('user/:userId')
   getGigsByUser(
     @Param('userId', ParseIntPipe) userId: number,
-    @Query() dto: SearchDto,
+    @Query() dto: SearchDtoNoName,
   ) {
     let { item, page } = dto;
     return this.gigsService.getGigsByUser(userId, item, page);
