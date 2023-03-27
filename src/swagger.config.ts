67aba3041d2877bfd7fd88a75c0224b05cc14ee3
AkiraVD <ledanhphuong@gmail.com>
2023-03-27 21:59:17 +0700

    Add link Video to swagger doc

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -4,4 +4,8 @@ export const SwggerConfig = new DocumentBuilder()
   .setTitle('NestJS - Fiverr API')
   .setDescription('Documentation of Backend API for Fiverr App')
   .addBearerAuth()
+  .setExternalDoc(
+    'Watch Video Demo',
+    'https://www.youtube.com/watch?v=cHkIkHzDq9ks',
+  )
   .build();
