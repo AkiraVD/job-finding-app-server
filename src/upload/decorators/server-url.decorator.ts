4e93e59324a5d030f122ed294f9ca25667b2e687
AkiraVD <ledanhphuong@gmail.com>
2023-03-27 08:18:30 +0700

    Update image link

--- src/upload/decorators/server-url.decorator.ts
+++ src/upload/decorators/server-url.decorator.ts
@@ -3,6 +3,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 export const ServerUrl = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    return `${req.protocol}://${req.get('host')}/`;
+    return `${req.protocol}://${req.get('host')}/public/images/`;
   },
 );
